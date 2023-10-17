2d182d29777a0cab79a24e13dd00bdee38a25b43
Ragesg <144818724+Ragesg@users.noreply.github.com>
2023-10-17 10:30:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -120,7 +120,7 @@ async def next_page(bot, query):
         ]
     elif settings['button'] and not ENABLE_SHORTLINK:
         btn = [
-            text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+            text == f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
         for file in files
         ] 
     else:
