38ef358ac19e24abe9ca2141690594d1535993ae
Rahul <144818724+Ragesg@users.noreply.github.com>
2023-11-06 12:12:47 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -23,7 +23,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('Support 📢', url=f'https://t.me/allfilmbots')
+                InlineKeyboardButton('Support 📢', url="t.me/webotz")
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             await bot.leave_chat(message.chat.id)
             return
         buttons = [[
-                          InlineKeyboardButton("Updates 📢", url=f'https://t.me/allfilmbots')
+                          InlineKeyboardButton("Updates 📢", url="t.me/webotz")
         ]] 
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
@@ -58,7 +58,7 @@ async def save_group(bot, message):
                                                  caption=(script.MELCOW_ENG.format(u.mention, message.chat.title)),
                                                  reply_markup=InlineKeyboardMarkup(
                                                      [[
-                                                         InlineKeyboardButton("Updates 📢", url=f'https://t.me/allfilmbots')
+                                                         InlineKeyboardButton("Updates 📢", url="t.me/webotz")
                                                      ]]
                                                  ),
                                                  parse_mode=enums.ParseMode.HTML
@@ -83,7 +83,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Updates 📢', url=f'https://t.me/allfilmbots')
+            InlineKeyboardButton('Updates 📢', url="t.me/webotz")
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -122,7 +122,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Updates 📢', url=f'https://t.me/allfilmbots')
+            InlineKeyboardButton('Updates 📢', url="t.me/webotz")
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
